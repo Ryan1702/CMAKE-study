0bdab73eba4ce7b455fd0ff7a8323de922e26e41
Ryan1702 <1545670295@qq.com>
2026-05-21 22:58:20 +0800

    修改字符串判定逻辑

--- content/v5/check/check.cpp
+++ content/v5/check/check.cpp
@@ -1,7 +1,8 @@
 #include<cstring>
 bool check(char s[])
 {
+    s[strcspn(s,"\n")]='\0';
     int len=strlen(s);
-    if(len!=2) return false;
+    if(len!=1) return false;
     return s[0]=='+'||s[0]=='-'||s[0]=='*'||s[0]=='/';
 }
